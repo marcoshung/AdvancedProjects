b64db28bd60adf518658c0406b44ee379fbd9906
marcoshung <marcoshung@10.250.97.44>
2018-08-29 18:26:22 -0700

    basics completed

--- Numeric Converter/src/Converter.java
+++ Numeric Converter/src/Converter.java
@@ -1,6 +1,43 @@
+import java.util.*;
 
 public class Converter {
+	
 	public static void main(String[] args) {
-		
+		boolean running = true;
+		Scanner console = new Scanner(System.in);
+		System.out.println("Enter whatever you wish to convert to binary");
+		while(running) {
+			String input = console.nextLine();
+			if(input.toLowerCase().equals("quit")) {
+				running = false;
+				System.out.println("YOU ARE DONE");
+			}else {
+				System.out.println(convert(input));
+			}
+		}
+	}
+	
+	public static String convert(String input) {
+		String[] words = input.split(" ");
+		String convertedString = "";
+		for(int i = 0; i < words.length; i++) {
+			String convertedWord = "";
+			
+			for(int j = 0; j < words[i].length(); j++) {
+				int numVal = (int) (words[i].charAt(j));
+				
+				for(int k = 128; k > 0; k /= 2) {
+					if(numVal > k) {
+						convertedWord += "1";
+						numVal -= k;
+					}else {
+						convertedWord += "0";
+					}
+				}
+				
+			}
+			convertedString += convertedWord;
+		}
+		return convertedString;
 	}
 }
